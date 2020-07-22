b16345e519b5188a9ecfaa3b9645a21b5a834fa3
PaulCrtr <paul.th.cartier@gmail.com>
2020-07-22 15:09:00 +0200

    roads done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./components/Home/Home";
+import Recipe from "./components/Recipe/Recipe";
+import PostRecipe from "./components/PostRecipe/PostRecipe";
 import "./App.css";
 
 function App() {
-  return <div className="App">Test...</div>;
+  return (
+    <div className="App">
+      <Nav />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/recipe/:id" component={Recipe} />
+          <Route exact path="/postrecipe" component={PostRecipe} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
