9c9f916a79531c9d186aae873a891283edc162cb
PaulCrtr <paul.th.cartier@gmail.com>
2020-07-22 21:40:39 +0200

    post component done

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,26 +3,26 @@ import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-      <Link to="/" class="navbar-brand">
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+      <Link to="/" className="navbar-brand">
         Recipes
       </Link>
       <button
-        class="navbar-toggler"
+        className="navbar-toggler"
         data-toggle="collapse"
         data-target="#navbarMenu"
       >
-        <span class="navbar-toggler-icon"></span>
+        <span className="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarMenu">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <Link to="/postrecipe" class="nav-link">
+      <div className="collapse navbar-collapse" id="navbarMenu">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link to="/postrecipe" className="nav-link">
               Post
             </Link>
           </li>
-          <li class="nav-item">
-            <Link to="/About" class="nav-link">
+          <li className="nav-item">
+            <Link to="/About" className="nav-link">
               About
             </Link>
           </li>
