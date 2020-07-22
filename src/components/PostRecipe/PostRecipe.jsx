0613ea800e2ed8ad0e85490b1a43c29355fd7a04
PaulCrtr <paul.th.cartier@gmail.com>
2020-07-23 00:55:30 +0200

    post request done

--- src/components/PostRecipe/PostRecipe.jsx
+++ src/components/PostRecipe/PostRecipe.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import IngredientsDisplay from "./IngredientsDisplay";
 import InstructionsDisplay from "./InstructionsDisplay";
 import Inputs from "./Inputs";
+import axios from "axios";
 
 const PostRecipe = () => {
   const [title, setTitle] = useState("");
@@ -13,6 +14,25 @@ const PostRecipe = () => {
   const [currentInstr, setCurrentInstr] = useState("");
   const [ingredients, setIngredients] = useState([]);
   const [instructions, setInstructions] = useState([]);
+
+  const sendRecipe = () => {
+    const tmpIngredients = [...ingredients].map((e) => [e]);
+    const tmpInstructions = [...instructions].map((e, i) => [e, i]);
+    const data = {
+      name_recipe: title,
+      time_recipe: necessaryTime,
+      number_recipe: nbPeople,
+      image: imageUrl,
+      author: author,
+      ingredients: tmpIngredients,
+      instructions: tmpInstructions,
+    };
+    console.log(data);
+    axios
+      .post("http://localhost:8000/recipes", data)
+      .then((res) => console.log(res.data))
+      .catch((error) => console.log(error));
+  };
 
   return (
     <form className="container col-12 col-sm-9 col-md-7 col-lg-6 col-xl-5">
@@ -48,6 +68,7 @@ const PostRecipe = () => {
         className="btn btn-dark form-control mt-3 mb-5"
         type="button"
         value="Send"
+        onClick={() => sendRecipe()}
       />
     </form>
   );
